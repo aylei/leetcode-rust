d5cf820ddf390ca93c44d93851918072a1921759
Yeh-lei Wu <rayingecho@gmail.com>
2019-01-18 00:06:37 +0800

    solve #19 #20

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,5 @@ mod n0015_3sum;
 mod n0016_3sum_closest;
 mod n0017_letter_combinations_of_a_phone_number;
 mod n0018_4sum;
+mod n0019_remove_nth_node_from_end_of_list;
+mod n0020_valid_parentheses;
---
+++ src/n0020_valid_parentheses.rs
@@ -0,0 +1,91 @@
+/**
+ * [20] Valid Parentheses
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * <ol>
+ * 	Open brackets must be closed by the same type of brackets.
+ * 	Open brackets must be closed in the correct order.
+ * </ol>
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<char> = Vec::new();
+        for ch in s.chars().into_iter() {
+            match stack.last() {
+                None => {},
+                Some(&last) => {
+                    if Solution::pair(last, ch) {
+                        stack.pop();
+                        continue
+                    }
+                },
+            }
+            stack.push(ch);
+        }
+        stack.is_empty()
+    }
+
+    #[inline(always)]
+    fn pair(open: char, close: char) -> bool {
+        (open == '{' && close == '}') ||
+            (open == '(' && close == ')') ||
+            (open == '[' && close == ']')
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_20() {
+        assert_eq!(Solution::is_valid("()[]{}".to_string()), true);
+    }
+}
