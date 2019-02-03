980c4bb9b04706c8ed204cef2028ff1ab63b6e78
Yeh-lei Wu <rayingecho@gmail.com>
2019-02-03 21:29:48 +0800

    solve #77 and suppress warning of unused code

--- src/lib.rs
+++ src/lib.rs
@@ -76,3 +76,5 @@ mod n0072_edit_distance;
 mod n0073_set_matrix_zeroes;
 mod n0074_search_a_2d_matrix;
 mod n0075_sort_colors;
+mod n0076_minimum_window_substring;
+mod n0077_combinations;
---
+++ src/n0077_combinations.rs
@@ -0,0 +1,76 @@
+/**
+ * [77] Combinations
+ *
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: n = 4, k = 2
+ * Output:
+ * [
+ *   [2,4],
+ *   [3,4],
+ *   [2,3],
+ *   [1,2],
+ *   [1,3],
+ *   [1,4],
+ * ]
+ * 
+ * 
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        let mut res: Vec<Vec<i32>> = Vec::new();
+        Solution::backtrack(1, n, k, vec![], &mut res);
+        res
+    }
+
+    fn backtrack(start: i32, end: i32, k: i32, curr: Vec<i32>, result: &mut Vec<Vec<i32>>) {
+        if k < 1 {
+            result.push(curr);
+            return
+        }
+        if end - start + 1 < k {
+            // elements is not enough, return quickly
+            return
+        }
+        for i in start..end+1 {
+            let mut vec = curr.clone();
+            vec.push(i);
+            Solution::backtrack(i+1, end, k-1, vec, result);
+        }
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_77() {
+        assert_eq!(
+            Solution::combine(4, 2),
+            vec![vec![1, 2], vec![1, 3], vec![1, 4], vec![2, 3], vec![2, 4], vec![3, 4]]
+        );
+        assert_eq!(
+            Solution::combine(1, 1),
+            vec![vec![1]]
+        );
+        let empty: Vec<Vec<i32>> = vec![];
+        assert_eq!(
+            Solution::combine(0, 1),
+            empty
+        );
+        assert_eq!(
+            Solution::combine(2, 1),
+            vec![vec![1], vec![2]]
+        );
+    }
+}
