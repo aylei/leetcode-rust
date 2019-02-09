cce007779d4df114e4868b0ebc5a0552bb60e08b
Yeh-lei Wu <rayingecho@gmail.com>
2019-02-10 00:19:29 +0800

    refine #97

--- src/n0097_interleaving_string.rs
+++ src/n0097_interleaving_string.rs
@@ -24,24 +24,20 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
-        Solution::backtrack(&s1, &s2, &s3)
+        let mut cache = vec![vec![false;s2.len()+1];s1.len()+1];
+        Solution::dfs(&s1.chars().collect(),
+                      &s2.chars().collect(),
+                      &s3.chars().collect(), 0, 0, 0, &mut cache)
     }
 
-    fn backtrack(s1: &str, s2: &str, s3: &str) -> bool {
-        if s3.len() < 1 && s1.len() < 1 && s2.len() < 1 {
-            return true;
-        }
-        if s1.len() > 0 && s3.len() > 0 && &s1[0..1] == &s3[0..1] {
-            if Solution::backtrack(&s1[1..], s2, &s3[1..]) {
-                return true;
-            }
-        }
-        if s2.len() > 0 && s3.len() > 0 && &s2[0..1] == &s3[0..1] {
-            if Solution::backtrack(s1, &s2[1..], &s3[1..]) {
-                return true;
-            }
-        }
-        false
+    fn dfs(s1: &Vec<char>, s2: &Vec<char>, s3: &Vec<char>, i: usize, j: usize, k: usize, invalid: &mut Vec<Vec<bool>>) -> bool {
+        if invalid[i][j] { return false }
+        if i == s1.len() && j == s2.len() && k == s3.len() { return true }
+        let valid =
+            (i < s1.len() && k < s3.len() && s1[i] == s3[k] && Solution::dfs(s1,s2,s3,i+1,j,k+1,invalid)) ||
+            (j < s2.len() && k < s3.len() && s2[j] == s3[k] && Solution::dfs(s1,s2,s3,i,j+1,k+1,invalid));
+        if !valid { invalid[i][j] = true }
+        valid
     }
 }
 
