6c7f9b7e805f04f58a16fbb8e88e15b915a91d50
Yeh-lei Wu <rayingecho@gmail.com>
2019-02-16 22:19:19 +0800

    solve #122

---
+++ src/n0122_best_time_to_buy_and_sell_stock_ii.rs
@@ -0,0 +1,64 @@
+/**
+ * [122] Best Time to Buy and Sell Stock II
+ *
+ * Say you have an array for which the i^th element is the price of a given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ * 
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ *              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ *              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+ *              engaging multiple transactions at the same time. You must sell before buying again.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max = 0;
+        for i in 1..prices.len() {
+            if prices[i] > prices[i-1] {
+                max += prices[i] - prices[i-1];
+            }
+        }
+        max
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_122() {
+        assert_eq!(Solution::max_profit(vec![7,1,5,3,6,4]), 7);
+        assert_eq!(Solution::max_profit(vec![1,2,3,4,5]), 4);
+    }
+}
