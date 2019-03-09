994733363416cfd31eda6be54bff0cc4eb9ce22a
Aylei <rayingecho@gmail.com>
2019-03-09 21:15:16 +0800

    solve #153

--- src/lib.rs
+++ src/lib.rs
@@ -147,3 +147,4 @@ mod n0149_max_points_on_a_line;
 mod n0150_evaluate_reverse_polish_notation;
 mod n0151_reverse_words_in_a_string;
 mod n0152_maximum_product_subarray;
+mod n0153_find_minimum_in_rotated_sorted_array;
---
+++ src/n0153_find_minimum_in_rotated_sorted_array.rs
@@ -0,0 +1,59 @@
+/**
+ * [153] Find Minimum in Rotated Sorted Array
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * 
+ * (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+ * 
+ * Find the minimum element.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,4,5,1,2] 
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,5,6,7,0,1,2]
+ * Output: 0
+ * 
+ * 
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut size = nums.len();
+        if size == 0 { return -1 }
+        let mut base = 0_usize;
+        while size > 1 {
+            let half = size / 2;
+            let mid = base + half;
+            if nums[mid] > nums[base] {
+                base = mid;
+            }
+            size -= half;
+        }
+        i32::min(nums[base], nums[(base + 1) % nums.len()])
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_153() {
+        assert_eq!(Solution::find_min(vec![4,5,6,1,2,3]), 1);
+        assert_eq!(Solution::find_min(vec![4,5,6,7,0,1,2]), 0);
+    }
+}
