c03b439cc4edb8eb9fd82637dc8ba4056ec91839
Aylei <rayingecho@gmail.com>
2019-05-03 23:32:14 +0800

    Solve #223

--- src/n0223_rectangle_area.rs
+++ src/n0223_rectangle_area.rs
@@ -22,9 +22,18 @@ pub struct Solution {}
 
 // submission codes start here
 
+// mention the integer divition
 impl Solution {
     pub fn compute_area(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32) -> i32 {
-        0
+        let center1 = ((a+c), (b+d));
+        let center2 = ((e+g), (f+h));
+        let rect1 = (c-a,d-b);
+        let rect2 = (g-e,h-f);
+        let x_intersect = i32::min((rect1.0 + rect2.0 - (center1.0-center2.0).abs()), i32::min(2 * rect1.0, 2 * rect2.0)) / 2;
+        let x_intersect = if x_intersect < 0 { 0 } else { x_intersect };
+        let y_intersect = i32::min((rect1.1 + rect2.1 - (center1.1-center2.1).abs()), i32::min(2 * rect1.1, 2 * rect2.1)) / 2;
+        let y_intersect = if y_intersect < 0 { 0 } else { y_intersect };
+        rect1.0 * rect1.1 - x_intersect * y_intersect + rect2.0 * rect2.1
     }
 }
 
@@ -35,5 +44,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_223() {}
+    fn test_223() {
+        assert_eq!(Solution::compute_area(0, 0, 0, 0, 0, 0, 0, 0), 0);
+        assert_eq!(Solution::compute_area(-3, 0, 3, 4, 0, -1, 9, 2), 45);
+        assert_eq!(Solution::compute_area(-2, -2, 2, 2, -2, -2, 2, 2), 16);
+        assert_eq!(Solution::compute_area(-2, -2, 2, 2, -1, 4, 1, 6), 20);
+    }
 }
