9dbedce5b7c2efba5f7cbda32498795089a161c9
Aylei <rayingecho@gmail.com>
2019-05-12 22:56:51 +0800

    Solve #231

--- src/lib.rs
+++ src/lib.rs
@@ -198,3 +198,4 @@ mod n0227_basic_calculator_ii;
 mod n0228_summary_ranges;
 mod n0229_majority_element_ii;
 mod n0230_kth_smallest_element_in_a_bst;
+mod n0231_power_of_two;
---
+++ src/n0231_power_of_two.rs
@@ -0,0 +1,51 @@
+/**
+ * [231] Power of Two
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1
+ * Output: true
+ * Explanation: 2^0 = 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 16
+ * Output: true
+ * Explanation: 2^4 = 16
+ *
+ * Example 3:
+ *
+ *
+ * Input: 218
+ * Output: false
+ *
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        let results: Vec<i32> = (0..31).map(|x| 2_i32.pow(x)).collect();
+        results.binary_search(&n).is_ok()
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_231() {
+        assert_eq!(Solution::is_power_of_two(-1), false);
+        assert_eq!(Solution::is_power_of_two(1), true);
+        assert_eq!(Solution::is_power_of_two(1024), true);
+    }
+}
